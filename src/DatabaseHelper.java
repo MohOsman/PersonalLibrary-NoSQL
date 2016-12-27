92205870c33ad1c87f0bbe973fd614b76d032254
Ayaz Ali <ayaz.ujjan@gmail.com>
2016-12-27 17:17:50 +0100

    Delete book with edition is aloso com+pleted but not tested

--- src/DatabaseHelper.java
+++ src/DatabaseHelper.java
@@ -104,6 +104,24 @@ public class DatabaseHelper {
         }
         return false;
     }
+    public boolean bookExistsWithEdition(String title, String author, int edition) {
+        System.out.println(" Start here ");
+        BasicDBObject bookQuery = new BasicDBObject();
+
+        BasicDBObject[] obj1 = new BasicDBObject[3];
+        obj1[0] = (new BasicDBObject("title", Pattern.compile(title, Pattern.CASE_INSENSITIVE)));
+        obj1[1] = (new BasicDBObject("author", Pattern.compile(author, Pattern.CASE_INSENSITIVE)));
+        obj1[2] = (new BasicDBObject("edition", edition ));
+        bookQuery.put("$and", obj1);
+
+        DBCursor cursor = dbCollectionBooks.find(bookQuery);
+        System.out.println("here");
+        if (cursor.hasNext()) {
+            System.out.println(" and in here");
+            return true;
+        }
+        return false;
+    }
 
     public DBCursor getbooksbyAuthor(String author) {
         BasicDBObject regexQuery = new BasicDBObject();
@@ -133,6 +151,22 @@ public class DatabaseHelper {
         catQuery.put("$and", obj1);
 
          dbCollectionBooks.remove(catQuery);
+
+
+
+
+    }
+
+    public void deletBookBytitleAndAuthorAndEdition(String title, String author, int edition){
+        BasicDBObject catQuery = new BasicDBObject();
+
+        BasicDBObject[] obj1 = new BasicDBObject[3];
+        obj1[0] = (new BasicDBObject("title", Pattern.compile(title , Pattern.CASE_INSENSITIVE)));
+        obj1[1] = (new BasicDBObject("author", Pattern.compile(author , Pattern.CASE_INSENSITIVE)));
+        obj1[2] = (new BasicDBObject("edition", edition ));
+        catQuery.put("$and", obj1);
+
+        dbCollectionBooks.remove(catQuery);
 
 
 
