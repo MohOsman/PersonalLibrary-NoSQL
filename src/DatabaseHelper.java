c3e74ef7ca60b4585f1620363ed653a863368808
Mohamed Osman <my222au@student.lnu.se>
2016-12-27 12:42:27 +0100

    Added some code to databaseHelper

--- src/DatabaseHelper.java
+++ src/DatabaseHelper.java
@@ -8,26 +8,38 @@ import java.util.List;
 
 
 public class DatabaseHelper {
+    private MongoClient client;
+    private DBCollection dbCollectionBooks;
+    private DBCollection dbColectionAuthors;
+    private DB db;
 
-    public static void main( String args[] ) {
 
+    public DatabaseHelper() {
+        client = new MongoClient("localhost" , 27017);
+        db = client.getDB("personalLibrary");
+        createCollection();
 
-
-        try{
-
-// Since 2.10.0, uses MongoClient
-            MongoClient mongo = new MongoClient( "localhost" , 27017 );
-
-            // Now connect to your databases
-            DB db = mongo.getDB("test");
-
-            System.out.println("Connect to database successfully");
-
-            DBCollection coll = db.getCollection("testCollection");
-            System.out.println(coll.count());
-
-        }catch(Exception e){
-            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-        }
     }
+
+
+    private void createCollection(){
+        dbCollectionBooks = db.getCollection("Books");
+        dbColectionAuthors = db.getCollection("Authors");
+
+    }
+
+    public  void createDocumnet (){
+     BasicDBObject dbObject = new BasicDBObject();
+        dbCollectionBooks.insert();
+
+
+
+
+    }
+
+
+
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 /**
  * Created by MohamedOsman on 2016-12-27.
  */
-//public class Main {
-//    public static void main(String[] args) {
-//        View view = new View();
-//        User user = new User(view);
-//    }
-//}
+public class Main {
+    public static void main(String[] args) {
+        View view = new View();
+        User user = new User(view);
+    }
+}
